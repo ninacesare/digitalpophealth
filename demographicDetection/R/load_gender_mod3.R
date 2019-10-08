eaa5d9eb4ee29d7c38b09dc8cb27335750ae5cda
Cesare <ncesare@bu.edu>
2019-10-08 18:42:32 -0400

    function to load gender method 3 model

---
+++ demographicDetection/R/load_gender_mod3.R
@@ -0,0 +1,9 @@
+#' Load pretrained gender classifier for gender method 3
+#'
+#' @format Pretrained model for gender classifier 3
+#' @keywords datasets
+#' @references Cesare et al
+#' @source gender_training_premade.rda
+#'
+
+load("/data/genderMethod3_mod.rda") 
