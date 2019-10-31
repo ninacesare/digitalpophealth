c2aa0f5c372bed76476a961d407a2a6097570b75
Cesare <ncesare@bu.edu>
2019-10-31 13:36:19 -0400

    removed functions to load trained model sinternally

--- demographicDetection/R/load_gender_mod3.R
+++
@@ -1,9 +0,0 @@
-#' Load pretrained gender classifier for gender method 3
-#'
-#' @format Pretrained model for gender classifier 3
-#' @keywords datasets
-#' @references Cesare et al
-#' @source gender_training_premade.rda
-#'
-
-load("/data/genderMethod3_mod.rda") 
